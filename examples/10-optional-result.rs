eba3155d52e39380718030eadef7007f8cc140bb
Eduard Alex Mihai <E.Mihai@student.tudelft.nl>
2023-09-29 19:48:16 +0200

    following examples

--- examples/10-optional-result.rs
+++ examples/10-optional-result.rs
@@ -10,7 +10,7 @@ fn find_nth_word(s: &str, n: usize) -> Option<&str> {
 fn main() {
     let word = find_nth_word("The quick brown fox", 3);
     match word {
-        Some(w) => println!("The 3rd word in 'The quick brown fox' is '{}'", w),
-        None => println!("No 3rd word in 'The quick brown fox'"),
+        Some(w) => println!("The 3rd word in 'The quick brown fox' is '{}'.", w),
+        None => println!("No 3rd word in 'The quick brown fox'."),
     }
 }
